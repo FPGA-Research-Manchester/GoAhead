ec1861b37f704922491a7f6c5eef64e5bbeff92e
dianalina99 <diana.dobrescu100@gmail.com>
2020-09-08 16:55:52 +0100

    Partially solve issue.

--- GoAhead/Commands/GridStyle/PrintPartitionPinConstraintsForSelection.cs
+++ GoAhead/Commands/GridStyle/PrintPartitionPinConstraintsForSelection.cs
@@ -60,22 +60,35 @@ namespace GoAhead.Commands.GridStyle
             }
 
             int startIndex = IndexOffset;
+            int startIndexTemp = 0;
 
             foreach (Tile t in tilesInFinalOrder)
             {
+                if (startIndexTemp >= NumberOfSignals)
+                {
+                    break;
+                }
+
                 SwitchboxName = t.Location;
                 StartIndex = startIndex;
                 SignalsForTile = MaxSignalsPerTile;
-
+                /*
                 if (NumberOfSignals - startIndex < MaxSignalsPerTile)
                 {
                     SignalsForTile = NumberOfSignals - startIndex;
+                    Console.WriteLine("INFO: There are " + (MaxSignalsPerTile - SignalsForTile) + " signals remaining in " + SwitchboxName);
+                }*/
+                
+                if (NumberOfSignals - startIndexTemp < MaxSignalsPerTile)
+                {
+                    SignalsForTile = NumberOfSignals - startIndexTemp;
                     Console.WriteLine("INFO: There are " + (MaxSignalsPerTile - SignalsForTile) + " signals remaining in " + SwitchboxName);
                 }
 
                 base.DoCommandAction();
 
                 startIndex += MaxSignalsPerTile;
+                startIndexTemp += MaxSignalsPerTile;
             }
         }
 
